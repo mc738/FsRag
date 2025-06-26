00bdfaf27cecc41042a46ab1db7ab4cf32de2a4a
Max <me@mclifford.dev>
2025-06-26 20:39:18 +0100

    chore(core): Initial work

---
+++ FsRag.ConsoleApp/Program.fs
@@ -0,0 +1,14 @@
+﻿open Microsoft.SemanticKernel
+
+
+let build () =
+    let kernel =
+        Kernel
+            .CreateBuilder()
+            .AddOllamaChatClient("")
+            
+    
+    ()
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
